078107a0019c8c41fccc79235182d9e0719445a8
Lucian Mihalca <infolucianmihalca@gmail.com>
2025-12-11 22:47:07 +0100

    feat: integrate TaskForms into the main view

---
+++ src/components/TaskForm.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+  import { ref } from 'vue'
+
+  const newTask = ref('')
+  const fromSubmitted = () => {
+    console.log(newTask.value)
+  }
+</script>
+
+<template>
+  <form @submit.prevent="fromSubmitted">
+    <label>
+      New Task
+      <input
+        v-model="newTask"
+        name="newTask"
+      />
+    </label>
+    <div class="button-container">
+      <button>Add</button>
+    </div>
+  </form>
+</template>
+
+<style scoped>
+  .button-container {
+    display: flex;
+    justify-content: end;
+  }
+</style>
